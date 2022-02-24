eb423bc628ecfbe250ac7db8505a2e9f9a5532d9
Keana23 <100366128+Keana23@users.noreply.github.com>
2022-02-24 10:16:23 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.House)
+    music.ringTone(392)
+})
+basic.showIcon(IconNames.TShirt)
+music.playMelody("C5 C5 A B B E D C ", 120)
